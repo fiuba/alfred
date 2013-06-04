433d7a8286d7278f1b1ca61c59888342226dc28e
NicoPaez <nicopaez@gmail.com>
2013-06-04 12:26:13 -0300

    Project structure clean up

--- app/app.rb
+++ app/app.rb
@@ -60,25 +60,8 @@ module Alfred
     #
 
     #set :allow_disabled_csrf, true
-
-    configure :development, :travis do
-      use OmniAuth::Builder do
-        provider :developer
-      end
-      set :login_page, "/login"    
-      ENV['APP_URL'] = 'http://127.0.0.1:3000/'
-    end
-    
-    configure :staging, :production do
-      use OmniAuth::Builder do
-        provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_SECRET_KEY'] 
-      end
-      set :login_page, "/auth/twitter"    
-    end
-    
-    access_control.roles_for :any do |role|
-        role.protect "/some-reserv-url"
-    end
+      
+    set :login_page, "/login"
 
     get '/' do
       render 'home/index'      
@@ -86,14 +69,6 @@ module Alfred
 
     get :login do
       render '/home/login'
-    end
-
-    get :auth, :map => '/auth/:provider/callback' do
-        auth    = request.env["omniauth.auth"]
-        account = Account.find_by_provider_and_uid(auth["provider"], auth["uid"]) || 
-                Account.create_with_omniauth(auth)
-        set_current_account(account)
-        redirect "/"
     end
 
   end

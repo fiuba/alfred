93a5d18503acedc53cb0ee565a07d6b1aab83880
NicoPaez <nicopaez@gmail.com>
2013-06-07 16:23:23 -0300

    DB config updated to use in-memory adapter for test and travis env.

--- config/database.rb
+++ config/database.rb
@@ -14,8 +14,8 @@ DataMapper::Property::String.length(255)
 
 case Padrino.env
   when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "alfred_development.db"))
-  when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "alfred_test.db"))
-  when :travis      then DataMapper.setup(:default, "mysql://root@localhost/alfred_test")
+  when :test        then DataMapper.setup(:default, "sqlite3::memory")
+  when :travis      then DataMapper.setup(:default, "sqlite3::memory")
  	when :staging 		then DataMapper.setup(:default, ENV['DATABASE_URL'])
   when :production  then DataMapper.setup(:default, ENV['DATABASE_URL'])
 end

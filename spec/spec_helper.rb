409b67cd92c4bb697409c757eefb59359d5487bf
Diego Sanchez <diego@apples-MacBook-Pro-2.local>
2013-06-08 08:58:50 -0300

    Capybara configuration added for rspec testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,14 @@
 PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
 require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+require 'capybara'
+require 'capybara/dsl'
+
+Capybara.app = Sinatra::Application
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
 	conf.before(:each) { DataMapper.auto_migrate! }
+	conf.include Capybara
 end
 
 def app

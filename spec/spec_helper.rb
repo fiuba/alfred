efb28bb04fa3e69f165b43f56985d2b3738197c9
Diego Sanchez <diego@apples-MacBook-Pro-2.local>
2013-06-08 07:41:54 -0300

    Test database reinitialization test database is reset for each test. It should help us to keep tests independent.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
+	conf.before(:each) { DataMapper.auto_migrate! }
 end
 
 def app

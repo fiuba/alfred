a002462d21241c8858c4e8e8886423398d9ad810
Diego Marcet <diego.marcet@tipit.net>
2013-06-08 19:38:34 -0300

    Force DataMapper to raise an exception if a non-mocked DB call is made

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,9 @@ RSpec.configure do |conf|
   conf.include Rack::Test::Methods
 	conf.before(:each) { DataMapper.auto_migrate! }
 	conf.include Capybara
+
+	# Raise exception if a DB call is not mocked
+	DataMapper.setup(:default, "abstract::")
 end
 
 def app

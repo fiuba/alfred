db0cfebe0beccacf7f57bed2562223c9717d318f
Ing. Diego Sánchez <diego.sanches@gmail.com>
2013-06-10 16:18:20 -0300

    rspec configuration changed - Test database initialization shifted to each rspec - Capybara.app points to Alfred::App

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 require 'capybara'
 require 'capybara/dsl'
 
-Capybara.app = Sinatra::Application
+Capybara.app = Alfred::App
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
-	conf.before(:each) { DataMapper.auto_migrate! }
 	conf.include Capybara
 end
 

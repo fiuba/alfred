24b9ff0338036ab129d217e028e43f63814628eb
Ing. Diego Sánchez <diego.sanches@gmail.com>
2013-06-11 16:55:00 -0300

    support path added to store rspec's helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 require 'capybara'
 require 'capybara/dsl'
 
+# RSpec's helpers 
+# It aims to add support for rspec tests
+Dir.glob(File.dirname(__FILE__) + "/support/**/*.rb").each {|f| require f}
+
+
 Capybara.app = Alfred::App
 
 RSpec.configure do |conf|

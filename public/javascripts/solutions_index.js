ed1fa4ec929762170f5ed14bed3cd14dfa316588
Diego Sanchez <diego@apples-MacBook-Pro-2.local>
2013-06-17 19:41:51 -0300

    Solution index page is intregrated with the new grid

---
+++ public/javascripts/solutions_index.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+				editableGrid = new EditableGrid("DemoGridAttach"); 
+
+				// we build and load the metadata in Javascript
+				editableGrid.load({ metadata: [
+					{ name: "File", datatype: "html" },
+					{ name: "Assingment", datatype: "string" },
+				]});
+
+				// then we attach to the HTML table and render it
+				editableGrid.attachToHTMLTable('solutionsGrid');
+
+				editableGrid.renderGrid();
+
+			} 

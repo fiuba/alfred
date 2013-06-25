7cec8a4114d9b3b7034474ba5bb2354ff4a85460
Diego Marcet <diego.marcet@tipit.net>
2013-06-25 08:34:40 -0300

    Removed generated tests for AssignmentGenericFile controller

--- spec/app/controllers/assignment_generic_file_controller_spec.rb
+++ spec/app/controllers/assignment_generic_file_controller_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
 describe "AssignmentGenericFileController" do
-  before do
-    get "/"
-  end
-
-  it "returns hello world" do
-    last_response.body.should == "Hello World"
-  end
+  
 end

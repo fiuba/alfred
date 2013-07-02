e9badc7b961c7720c51db348154a968a65236e6d
Ing. Diego Sánchez <diego.sanches@gmail.com>
2013-07-02 15:35:26 -0300

    Test added for Solution controller in order for it to deal successfully during dropbox connection failures

--- spec/app/controllers/solutions_controller_spec.rb
+++ spec/app/controllers/solutions_controller_spec.rb
@@ -8,21 +8,23 @@ describe "SolutionsController" do
   let(:account) { Factories::Account.student }
   let(:assignment) { Factories::Assignment.vending_machine}
 	
-	it "should query Solutions" do
-		Solution.should_receive(:all).and_return([])
-		Alfred::App.any_instance.should_receive(:render).with('solutions/index').and_return({})
-		get '/solutions'
+	describe "index" do
+		it "should query Solutions" do
+			Solution.should_receive(:all).and_return([])
+			Alfred::App.any_instance.should_receive(:render).with('solutions/index').and_return({})
+			get '/solutions'
+		end
 	end
 
-  describe "solution upload without erros" do
-    it "should add both solution and solution_generic_file" do
-      file_name = 'submission.st'
+	describe "create" do
+		before do
+      @file_name = 'submission.st'
       file_content = 'solution content'
-      params = { 
+      @params = { 
         :solution => { 
 
           :file => {
-            :filename => file_name, 
+            :filename => @file_name, 
             :tempfile => file_content
           },
           :account_id => account.id,
@@ -30,19 +32,41 @@ describe "SolutionsController" do
         }
       }
 
-      gateway = double(Storage::DropboxGateway)
-      gateway.should_receive(:upload).with('/solutions/1/submission.st', 
-        'solution content')
-      Storage::StorageGateways.should_receive(:get_gateway).and_return(gateway)
+      @gateway = double(Storage::DropboxGateway)
+      Storage::StorageGateways.should_receive(:get_gateway).and_return(@gateway)
+		end
 
-      post '/solutions/create', params
+		describe "with valid provided datas" do
+			before do
+      	@gateway.should_receive(:upload).with('/solutions/1/submission.st', 
+       		'solution content')
+			end
 
-      new_solution = Solution.all.first
-      generic_file = new_solution.solution_generic_files.first
+    	it "should add both solution and solution_generic_file" do
+				post '/solutions/create', @params
+
+      	new_solution = Solution.all.first
+      	generic_file = new_solution.solution_generic_files.first
       
-      new_solution.file.should == file_name
-      generic_file.solution.should == new_solution
+      	new_solution.file.should == @file_name
+      	generic_file.solution.should == new_solution
+    	end
+		end
 
-    end
-  end
+		describe "with dropbox connection problems" do
+			before do
+				Alfred::App.any_instance.stub(:current_account).and_return(account)
+				error = Storage::FileUploadFailedError.new(StandardError.new)
+      	@gateway.should_receive(:upload)
+					.with('/solutions/1/submission.st', 'solution content')
+					.and_raise(error)
+			end
+
+    	it "should not add either solution or solution_generic_file" do
+				post '/solutions/create', @params
+      	Solution.all.size.should be_equal(0)
+    	end
+
+		end
+	end
 end

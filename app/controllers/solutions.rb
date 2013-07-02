288dc62e792a78771a380ec9577a09e48a561ac5
Ing. Diego Sánchez <diego.sanches@gmail.com>
2013-07-02 16:47:13 -0300

    Solution cannot be updated

--- app/controllers/solutions.rb
+++ app/controllers/solutions.rb
@@ -61,25 +61,6 @@ Alfred::App.controllers :solutions do
     end
   end
 
-  put :update, :with => :id do
-    @title = pat(:update_title, :model => "solution #{params[:id]}")
-    @solution = Solution.get(params[:id].to_i)
-    if @solution
-      if @solution.update(params[:solution])
-        flash[:success] = pat(:update_success, :model => 'Solution', :id =>  "#{params[:id]}")
-        params[:save_and_continue] ?
-          redirect(url(:solutions, :index)) :
-          redirect(url(:solutions, :edit, :id => @solution.id))
-      else
-        flash.now[:error] = pat(:update_error, :model => 'solution')
-        render 'solutions/edit'
-      end
-    else
-      flash[:warning] = pat(:update_warning, :model => 'solution', :id => "#{params[:id]}")
-      halt 404
-    end
-  end
-
   delete :destroy, :with => :id do
     @title = "Solutions"
     solution = Solution.get(params[:id].to_i)

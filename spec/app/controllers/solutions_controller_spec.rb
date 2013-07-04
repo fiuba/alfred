e15e99a5fec432df36f936dd0631d21b5b73b6f8
Ing. Diego Sánchez <diego.sanches@gmail.com>
2013-07-04 16:48:37 -0300

    Solution controller supports deletions

--- spec/app/controllers/solutions_controller_spec.rb
+++ spec/app/controllers/solutions_controller_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 describe "SolutionsController" do
   before (:each) do
     DataMapper.auto_migrate!
+
+     @gateway = double(Storage::DropboxGateway)
+     Storage::StorageGateways.stub(:get_gateway)
+     	.and_return(@gateway)
   end
 
-  let(:account) { Factories::Account.student }
+  let(:student) { Factories::Account.student }
+  let(:another_student) { Factories::Account.student( "Bruce", "b@d.com" ) }
   let(:assignment) { Factories::Assignment.vending_machine}
 	
 	describe "index" do
@@ -27,12 +32,11 @@ describe "SolutionsController" do
             :filename => @file_name, 
             :tempfile => file_content
           },
-          :account_id => account.id,
+          :account_id => student.id,
           :assignment_id => assignment.id
         }
       }
 
-      @gateway = double(Storage::DropboxGateway)
       Storage::StorageGateways.should_receive(:get_gateway).and_return(@gateway)
 		end
 
@@ -55,7 +59,7 @@ describe "SolutionsController" do
 
 		describe "with dropbox connection problems" do
 			before do
-				Alfred::App.any_instance.stub(:current_account).and_return(account)
+				Alfred::App.any_instance.stub(:current_account).and_return(student)
 				error = Storage::FileUploadFailedError.new(StandardError.new)
       	@gateway.should_receive(:upload)
 					.with('/solutions/1/submission.st', 'solution content')
@@ -65,8 +69,55 @@ describe "SolutionsController" do
     	it "should not add either solution or solution_generic_file" do
 				post '/solutions/create', @params
       	Solution.all.size.should be_equal(0)
+				SolutionGenericFile.all.size.should be_equal(0)
     	end
 
+		end
+	end
+
+	describe "destroy" do
+		before do
+			@solution = Factories::Solution.for( 
+				Factories::Assignment.vending_machine )
+			@solution_to_delete = Factories::Solution.for( 
+				Factories::Assignment.vending_machine )
+			@file_name = @solution_to_delete.file
+
+			Alfred::App.any_instance.stub(:current_account)
+      	.and_return(@solution_to_delete.account)
+		end
+
+		describe "when storage file error arise" do
+			it "should not remove either solution, solution_generic_file and storage_file" do
+				error = Storage::FileDeleteError.new(StandardError.new)
+ 		    @gateway.should_receive(:delete)
+        	.with("/solutions/#{@solution_to_delete.id}/#{@file_name}")
+					.and_raise(error)
+
+				expect { delete "/solutions/destroy/#{@solution_to_delete.id}" }
+					.not_to change{ Solution.all.size }.from(2).to(2)
+			end
+		end
+
+		it "should remove the solution, solution_generic_file and storage file" do
+      @gateway.should_receive(:delete)
+          .with("/solutions/#{@solution_to_delete.id}/#{@file_name}")
+
+			expect { delete "/solutions/destroy/#{@solution_to_delete.id}" }
+				.to change{ Solution.all.size }.from(2).to(1)
+		end
+
+		describe "unauthorized student tries to delete other's solution" do
+			before do
+				  Alfred::App.any_instance.stub(:current_account)
+            .and_return(another_student)
+			end
+
+			it "should not remove either the solution or solution_generic_files" do
+				expect { delete "/solutions/destroy/#{@solution_to_delete.id}" }
+					.not_to change{ Solution.all.size }.from(2).to(2)
+        last_response.status.should == 403
+			end
 		end
 	end
 
@@ -76,10 +127,6 @@ describe "SolutionsController" do
 				@new_solution = Factories::Solution.for( assignment )
 				@file_name = @new_solution.file
         @owner = @new_solution.account
-
-      	@gateway = double(Storage::DropboxGateway)
-      	Storage::StorageGateways.stub(:get_gateway)
-          .and_return(@gateway)
 
 				Alfred::App.any_instance.stub(:current_account)
           .and_return(@owner)
@@ -99,9 +146,8 @@ describe "SolutionsController" do
 
       describe "unauthorized student tries to download other's solution" do
         before do
-          @another_student = Factories::Account.student( "Bruce", "b@d.com" )
 				  Alfred::App.any_instance.stub(:current_account)
-            .and_return(@another_student)
+            .and_return(another_student)
         end
 
         it "should not respond solution belonging to other student" do

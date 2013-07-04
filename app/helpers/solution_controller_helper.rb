c75bbadd7540b0a16e79d8aed10cb6e766ab237d
Ing. Diego Sánchez <diego.sanches@gmail.com>
2013-07-04 16:56:50 -0300

    Solution controller helper added

---
+++ app/helpers/solution_controller_helper.rb
@@ -0,0 +1,14 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+Alfred::App.helpers do
+  def file_download_link(file_path)
+    storage_gateway = Storage::StorageGateways.get_gateway
+
+  	storage_gateway.share(file_path)[:url]
+  end
+
+	def conveys_warning( msg, warning_code )
+  	flash[:warning] = msg
+  	halt warning_code
+	end
+end

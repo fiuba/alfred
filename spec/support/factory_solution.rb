f5f5175bdb8eeee8ef0f871439618b7e0cbc9308
diego <diego.sanches@gmail.com>
2013-07-07 21:06:25 -0300

    Factories updated to suit testing of correction

--- spec/support/factory_solution.rb
+++ spec/support/factory_solution.rb
@@ -1,12 +1,27 @@
 module Factories
   module Solution
-    def self.for( anAssignment )
-      account = Factories::Account.student
+    def self.for( assignment)
+
+      author = Factories::Account.student
       file_name = 'solution.st'
 
       new_solution = Alfred::App::Solution.create!( 
-        :file => file_name, :account => account,
-        :assignment => anAssignment )
+        :file => file_name, :account => author,
+        :assignment => assignment )
+
+			Alfred::App::SolutionGenericFile.create!( 
+        :solution => new_solution, :name => file_name )
+
+      new_solution
+    end
+
+    def self.forBy( assignment, account = nil)
+      author = account || Factories::Account.student
+      file_name = 'solution.st'
+
+      new_solution = Alfred::App::Solution.create!( 
+        :file => file_name, :account => author,
+        :assignment => assignment )
 
 			Alfred::App::SolutionGenericFile.create!( 
         :solution => new_solution, :name => file_name )

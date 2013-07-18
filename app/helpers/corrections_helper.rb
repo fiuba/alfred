bdbd8c664eb0477f8abad9cf855ecc1b676043f9
diego <diego.sanches@gmail.com>
2013-07-18 20:01:03 -0300

    Trello#5 - As a teacher I rank a solution submitted by a student

--- app/helpers/corrections_helper.rb
+++ app/helpers/corrections_helper.rb
@@ -1,7 +1,22 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Alfred::App.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+  def solution_file_url( correction ) 
+    solution = correction.solution
+    assignment = solution.assignment
+    url(:solutions, assignment.id, :file, solution.id)
+  end
+
+  def assignment_and_author_information( c )
+    student = c.solution.account
+    assignment_title = @correction.solution.assignment.name
+    author = "#{student.surname}, #{student.name} ( #{student.buid} )"
+    "#{assignment_title} - #{author}"
+  end
+
+  def solution_abridged_info( solution )
+    submission_date = solution.created_at
+    "#{t('corrections.submission_date')}: #{submission_date.strftime("%Y-%m-%d %H:%M:%S")}"
+  end
+
 end
--- spec/app/controllers/corrections_controller_spec.rb
+++ spec/app/controllers/corrections_controller_spec.rb
@@ -1,28 +1,100 @@
 require 'spec_helper'
 
 describe "CorrectionsController" do
- 
+  let(:teacher) { Factories::Account.teacher }
+  let(:algorithm) { Factories::Course.algorithm }
+  let(:solution) { Factories::Solution.for( Factories::Assignment.vending_machine ) }
+
+	before (:each) do
+		DataMapper.auto_migrate!
+
+		Alfred::App.any_instance.stub(:current_account)
+      .and_return(Factories::Account.teacher)
+		Alfred::App.any_instance.stub(:current_course)
+      .and_return(Factories::Course.algorithm)
+	end
+
 	describe "index" do
 		it "should respond with error 403 whether not a teacher access to" do
-			# TODO: Review authorization logic
-			teacher = Factories::Account.teacher
-			student = Factories::Account.student
-			Alfred::App.any_instance.stub(:current_account).and_return(teacher)
-
-			get "/corrections/#{student.id}"
+		  Alfred::App.any_instance.stub(:current_account)
+        .and_return(Factories::Account.student)
+			get "/course/#{algorithm.id}/corrections"
 			last_response.status.should == 403
 		end
 
 		it "should render index content" do
-			teacher = Factories::Account.teacher
-			Alfred::App.any_instance.stub(:current_account).and_return(teacher)
-
 			Correction.should_receive(:all)
 				.with(:teacher => teacher)
 				.and_return([])
 			Alfred::App.any_instance.should_receive(:render)
 				.with('corrections/index').and_return({})
-			get "/corrections/#{teacher.id}"
+			get "/course/#{algorithm.id}/corrections"
 		end
 	end
+
+	describe "create" do
+		before do
+    	@params = { 
+      	:correction => { 
+						:solution_id => solution.id
+				}
+			}
+		end
+
+		describe "student tries to create a solution" do
+			it "should response 403" do
+		    Alfred::App.any_instance.stub(:current_account)
+          .and_return(Factories::Account.student)
+				post "/course/#{algorithm.id}/corrections/create", @params
+				last_response.status.should == 403
+			end
+		end
+
+		describe "teacher creates a solution" do
+			it "should create a new correction" do
+				expect { post "/course/#{algorithm.id}/corrections/create", @params}
+					.to change{ Correction.all(:teacher => teacher).size }.from(0).to(1)
+			end
+		end
+	end
+
+	describe "edit" do 
+		it "should render index content" do
+			correction = Factories::Correction.correctsBy( solution, teacher )
+			Correction.should_receive(:get).with(correction.id)
+				.and_return(correction)
+			Alfred::App.any_instance.should_receive(:render)
+				.with('corrections/edit').and_return({})
+			get "/course/#{algorithm.id}/corrections/edit/#{correction.id}"
+		end
+	end
+
+  describe "update" do
+    before do
+			@correction = Factories::Correction.correctsBy( solution, teacher )
+    end
+
+    it "should change correction's datas" do
+      public_comments = "public new comments"
+      private_comments = "private new comments"
+
+    	@params = { 
+      	:correction => { 
+						:solution_id => solution.id,
+            :public_comments => public_comments,
+            :private_comments => private_comments,
+            :grade => 7
+				}
+			}
+
+      Correction.should_receive(:get).with(@correction.id)
+        .and_return(@correction)
+
+			put "/course/#{algorithm.id}/corrections/update/#{@correction.id}", @params
+
+      @correction.public_comments.should == public_comments
+      @correction.private_comments.should == private_comments
+      @correction.grade.should == 7
+    end
+  end
 end

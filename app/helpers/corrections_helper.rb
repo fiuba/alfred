67028f54766064e44235bc705c5f9d9fce878062
Diego Marcet <diego.marcet@tipit.net>
2013-07-19 10:09:47 -0300

    Fixed assignment status translation on CorrectionsHelper

--- app/helpers/corrections_helper.rb
+++ app/helpers/corrections_helper.rb
@@ -1,13 +1,5 @@
 Alfred::App.helpers do
-	LABELS_FOR_STATUS = { 
-		:solution_pending => I18n.translate('assignments.status.solution_pending'),
-		:correction_pending => I18n.translate('assignments.status.correction_pending'),
-		:correction_in_progress => I18n.translate('assignments.status.correction_in_progress'),
-		:correction_passed => I18n.translate('assignments.status.correction_passed'),
-		:correction_failed => I18n.translate('assignments.status.correction_failed')
-	}
-
   def correction_status_label(status)
-  	LABELS_FOR_STATUS[status.to_sym]
+  	I18n.translate("assignments.status.#{status.to_sym}")
   end
 end

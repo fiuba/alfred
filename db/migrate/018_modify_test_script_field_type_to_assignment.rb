980b0b6a9c3c2b0db44bbe94b5ca46a8b24825c7
Diego Sanchez <diego.sanches@gmail.com>
2013-08-02 12:13:14 -0300

    Altering test_field's type from assignment

---
+++ db/migrate/018_modify_test_script_field_type_to_assignment.rb
@@ -0,0 +1,41 @@
+migration 18, :modify_test_script_field_type_to_assignment do
+  up do
+    # Sqlite doesn't support ALTERing tables to remove columns
+    if DataMapper.repository.adapter.class.to_s =~ /DataMapper::Adapters::SqliteAdapter/i
+      drop_table :assignments
+
+      create_table :assignments do
+        column :id, Integer, :serial => true
+        column :name, String, :length => 255
+        column :files, String, :length => 255
+        column :test_script, DataMapper::Property::Text
+        column :deadline, DateTime
+        column :course_id, Integer
+      end
+    else
+      modify_table :assignments do
+        change_column :test_script, DataMapper::Property::Text
+      end
+    end
+  end
+
+  down do
+    # Sqlite doesn't support ALTERing tables to remove columns
+    if DataMapper.repository.adapter.class.to_s =~ /DataMapper::Adapters::SqliteAdapter/i
+      drop_table :assignments
+
+      create_table :assignments do
+        column :id, Integer, :serial => true
+        column :name, String, :length => 255
+        column :files, String, :length => 255
+        column :test_script, DataMapper::Property::Text
+        column :deadline, DateTime
+        column :course_id, Integer
+      end
+    else
+      modify_table :assignments do
+        change_column :test_script, DataMapper::Property::String
+      end
+    end
+  end
+end

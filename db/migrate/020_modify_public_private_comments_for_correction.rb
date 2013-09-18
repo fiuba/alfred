c8a28a14a8fd4349405b0d314046d03fb97f81f3
diego <diego.sanches@gmail.com>
2013-09-18 17:18:05 -0300

    V2 - I've forgotten including migration for SQLitle

--- db/migrate/020_modify_public_private_comments_for_correction.rb
+++ db/migrate/020_modify_public_private_comments_for_correction.rb
@@ -1,21 +1,47 @@
 migration 20, :modify_public_private_comments_for_correction do
   up do
-     DataMapper.repository.adapter.execute(                                   \
-       'ALTER TABLE "corrections" ALTER COLUMN "public_comments" TYPE text'   \
-     )
+    if DataMapper.repository.adapter.class.to_s =~ /DataMapper::Adapters::SqliteAdapter/i
+      drop_table :corrections
 
-     DataMapper.repository.adapter.execute(                                   \
-       'ALTER TABLE "corrections" ALTER COLUMN "private_comments" TYPE text'  \
-     )
+      create_table :corrections do
+        column :id, Integer, :serial => true
+        column :public_comments, Text
+        column :private_comments, Text
+        column :grade, Float
+        column :created_at, DateTime  
+        column :updated_at, DateTime
+      end
+    else
+      DataMapper.repository.adapter.execute(                                   \
+        'ALTER TABLE "corrections" ALTER COLUMN "public_comments" TYPE text'   \
+      )
+     
+      DataMapper.repository.adapter.execute(                                   \
+        'ALTER TABLE "corrections" ALTER COLUMN "private_comments" TYPE text'  \
+      )
+    end
   end
 
   down do
-     DataMapper.repository.adapter.execute(                                           \
-       'ALTER TABLE "corrections" ALTER COLUMN "public_comments" TYPE VARCHAR(255)'   \
-     )
+    if DataMapper.repository.adapter.class.to_s =~ /DataMapper::Adapters::SqliteAdapter/i
+      drop_table :corrections
 
-     DataMapper.repository.adapter.execute(                                           \
-       'ALTER TABLE "corrections" ALTER COLUMN "private_comments" TYPE VARCHAR(255)'  \
-     )
+    create_table :corrections do
+      column :id, Integer, :serial => true
+      column :public_comments, String
+      column :private_comments, String
+      column :grade, Float
+      column :created_at, DateTime  
+      column :updated_at, DateTime
+    end
+    else
+      DataMapper.repository.adapter.execute(                                           \
+        'ALTER TABLE "corrections" ALTER COLUMN "public_comments" TYPE VARCHAR(255)'   \
+      )
+     
+      DataMapper.repository.adapter.execute(                                           \
+        'ALTER TABLE "corrections" ALTER COLUMN "private_comments" TYPE VARCHAR(255)'  \
+      )
+    end
   end
 end

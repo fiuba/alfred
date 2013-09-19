fedc36c86d618a4680c42d5725807fce2e1fb8cc
Diego Marcet <diego.marcet@tipit.net>
2013-09-19 12:40:30 -0300

    Added teacher's name on from field for notification emails

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -36,7 +36,7 @@
 Alfred::App.mailer :notification do
 
   email :correction_result do | correction |
-    from correction.teacher.email
+    from "#{correction.teacher.full_name} <#{correction.teacher.email}>"
     to correction.solution.account.email
     subject '[FIUBA-ALGO] Correccion de trabajo practico'
     locals :correction => correction
@@ -45,7 +45,7 @@ Alfred::App.mailer :notification do
   end
 
 	email :solution_test_result do | solution |
-    from ENV['MAIL_SUPPORT_ADDRESS']
+    from "Alfred <#{ENV['MAIL_SUPPORT_ADDRESS']}>"
     to solution.account.email
     subject '[FIUBA-ALGO] Resultado pruebas del trabajo practico'
     locals :solution => solution

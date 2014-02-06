c21a0246e2c66ad103009b45d87283f78ce88ad2
Diego Marcet <anero79@gmail.com>
2014-02-05 22:56:56 -0300

    Allow a single Course to be active at a time

--- models/course.rb
+++ models/course.rb
@@ -5,10 +5,17 @@ class Course
   property :active, Boolean
   has n, :accounts, :through => Resource
 
+  before :save do
+    # Allow a single course to be active
+    if self.active
+      Course.each { |c| c.update(active: false) if c != self  }
+    end
+  end
+
   def self.active
   	Course.find_by_active(true)
   end
-  
+
   def students
   	self.accounts.select { |a| a.is_student? }
   end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,16 +2,28 @@ require 'spec_helper'
 
 describe Course do
 
-	describe 'students' do
+  describe 'active' do
+    it "should allow a single course to be active at any time" do
+      course1 = Course.new(name: 'course 1', active: true)
+      course1.save.should be_true
 
+      course2 = Course.new(name: 'course 2', active: true)
+      course2.save.should be_true
+
+      course2.active.should be_true
+      course1.reload
+      course1.active.should be_false
+    end
+  end
+
+	describe 'students' do
 		it 'should query accounts with role student' do
-			course = Course.new 
+			course = Course.new
 			accounts = []
 			student = Account.new_student(:name => 'student name')
 			teacher = Account.new_teacher(:name => 'teacher name')
 			course.should_receive(:accounts).and_return([student, teacher])
 			course.students.size.should eq 1
 		end
-		
 	end
 end

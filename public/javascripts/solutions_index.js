d7ffb45a5c97d3414667a17a2bf86c1b41700617
NicoPaez <nicopaez@gmail.com>
2014-04-18 22:22:37 -0300

    Test output added

--- public/javascripts/solutions_index.js
+++ public/javascripts/solutions_index.js
@@ -1,17 +1,5 @@
 $(document).ready(function() {
-	// TODO: Fix editable grid
-
-	// editableGrid = new EditableGrid("DemoGridAttach");
-
-	// // we build and load the metadata in Javascript
-	// editableGrid.load({ metadata: [
-	// 	{ name: "File", datatype: "html" },
-	// 	{ name: "File", datatype: "html" },
-	// 	{ name: "Assingment", datatype: "string" },
-	// ]});
-
-	// // then we attach to the HTML table and render it
-	// editableGrid.attachToHTMLTable('solutionsGrid');
-
-	// editableGrid.renderGrid();
+	$('.icon-info-sign').click( function click() {
+			alert($(this).attr('data-content'));
+		});
 });

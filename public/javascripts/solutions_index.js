5917341f62fe572e56508421b4e109a9cfc6def7
NicoPaez <nicopaez@gmail.com>
2014-04-21 09:33:56 -0300

    Test_output escaped

--- public/javascripts/solutions_index.js
+++ public/javascripts/solutions_index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.icon-info-sign').click( function onInfoClick() {
+	$('.test-output').click( function onInfoClick() {
 			var data = $(this).attr('data-content');
 			$('#testOutput').text(data);
 			$('#myModal').modal('show');

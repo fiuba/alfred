ec5a07256f3b45cd29ddb3b66b8052887cbdd3e1
matiasmelendi <mmelendi@10pines.com>
2015-06-20 14:51:07 -0300

    * Raise exception if try to download an non-existent file

--- models/storage/local_gateway.rb
+++ models/storage/local_gateway.rb
@@ -1,6 +1,8 @@
 require 'fileutils'
+require 'exceptions/file_exceptions'
 
 class Storage::LocalGateway
+  include FileExceptions
 
   def upload(file_path, file)
     path = File.dirname(file_path)
@@ -11,6 +13,8 @@ class Storage::LocalGateway
   end
 
   def download file_path
+    verify_file_existence! file_path
+
     File.open(File.join(ENV["HOME_PATH"], file_path))
   end
 
@@ -23,6 +27,10 @@ class Storage::LocalGateway
     unless File.directory?(File.join(ENV["HOME_PATH"], path))
       FileUtils.mkdir_p(File.join(ENV["HOME_PATH"], path))
     end
+  end
+
+  def verify_file_existence! file_path
+    raise FileNotFound.new("File in #{file_path} not found") unless File.exists?(ENV["HOME_PATH"]+file_path)
   end
 
 end

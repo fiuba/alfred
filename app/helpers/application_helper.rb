eb9745a86d260fd1bd25bf8711d570fc774cdb7d
angeles-tellaarena <angeles.tellaarena@gmail.com>
2015-06-20 17:25:00 -0300

    Added new application helper (gravatar methods included)

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,17 @@
+Alfred::App.helpers do
+
+  def gravatar_url
+    gravatar_url_for current_account.email
+  end
+
+  def gravatar_url_for email
+    hash = gravatar_hash_for email
+    "http://www.gravatar.com/avatar/#{hash}"
+  end
+
+  private
+  def gravatar_hash_for email
+    Digest::MD5.hexdigest(email.delete(" ").downcase)
+  end
+
+end

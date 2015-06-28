dd13444dbf961398dea075358f2d44bb902379bd
matiasmelendi <mmelendi@10pines.com>
2015-06-28 20:22:47 -0300

    * Added some helper methods to use in assignment reports page

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -4,4 +4,31 @@ Alfred::App.helpers do
     @assignment.errors.key?(form_field) && @assignment.errors[form_field].count > 0
   end
 
+  def passed_for assignment
+    solutions_approved_for(assignment).count
+  end
+
+  def failed_for assignment
+    assignment.solutions.reject{ |solution| solution.correction.try(:approved?) }.count
+  end
+
+  def total_solutions_for assignment
+    assignment.solutions.count
+  end
+
+  def average_for assignment
+    if passed_for(assignment) > 0
+      approved_grades_sum(assignment) / passed_for(assignment)
+    else
+      "--"
+    end
+  end
+
+  def solutions_approved_for assignment
+    assignment.solutions.select{ |solution| solution.correction.try(:approved?) }
+  end
+
+  def approved_grades_sum(assignment)
+    solutions_approved_for(assignment).map{ |solution| solution.correction.grade }.reduce(:+) || 0
+  end
 end

8dd35c00f56b1ad80a5922781f3b7605320c6f31
matiasmelendi <mmelendi@10pines.com>
2015-07-28 20:01:44 -0300

    * show average and assignment report for a solution helper added

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -4,4 +4,12 @@ Alfred::App.helpers do
     @assignment.errors.key?(form_field) && @assignment.errors[form_field].count > 0
   end
 
+  def show_average average
+    return "--" if average == 0
+    average
+  end
+
+  def assignment_report assignment
+    AssignmentStatistics.new(assignment)
+  end
 end
